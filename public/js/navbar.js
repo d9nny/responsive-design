7b5129a9d5248590c1bca7744510c7a6f8db9c1d
danwheeler <da-wheeler@hotmail.co.uk>
2016-01-21 18:08:39 +0000

    jquery added for drop down menu

---
+++ public/js/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	$(function() {
+		var pull = $('#pull'),
+				menu = $('nav ul');
+
+		$(pull).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
+		});
+
+		$(window).resize(function() {
+			var w = $(window).width();
+			if( w > 320 && menu.is(':hidden')) {
+				menu.removeAttr('style');
+			}
+		});
+	});
+
+});
